64bbc975f2622ce272f95ac97cdb2126e95fe732
Julia Pap <papjuli@gmail.com>
2017-09-10 23:41:47 +0200

    Refactoring.

--- web/cloud.js
+++ web/cloud.js
@@ -1,22 +1,23 @@
+
 function handleClientLoad() {
-// Loads the client library and the auth2 library together for efficiency.
-// Loading the auth2 library is optional here since `gapi.client.init` function will load
-// it if not already loaded. Loading it upfront can save one network request.
-gapi.load('client:auth2', initClient);
+  // Loads the client library and the auth2 library together for efficiency.
+  // Loading the auth2 library is optional here since `gapi.client.init` function will load
+  // it if not already loaded. Loading it upfront can save one network request.
+  gapi.load('client:auth2', initClient);
 }
 
 function initClient() {
-// Initialize the client with API key and People API, and initialize OAuth with an
-// OAuth 2.0 client ID and scopes (space delimited string) to request access.
-gapi.client.init({
+  // Initialize the client with API key and People API, and initialize OAuth with an
+  // OAuth 2.0 client ID and scopes (space delimited string) to request access.
+  gapi.client.init({
     discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
     clientId: '544351928107-lnucrgo39ustk9aq28s7hnp9kdto6k5j.apps.googleusercontent.com',
     scope: 'https://www.googleapis.com/auth/spreadsheets'
-}).then(function() {
-    profile = myProfile();
+  }).then(function() {
+    var profile = myProfile();
     document.getElementById("whoami").innerHTML = profile.getEmail() + " " + profile.getName();
     loadBoardList();
-});
+  });
 }
 
 function myProfile() {
@@ -24,50 +25,53 @@ function myProfile() {
 }
 
 function handleSignInClick(event) {
-// Ideally the button should only show up after gapi.client.init finishes, so that this
-// handler won't be called before OAuth is initialized.
-gapi.auth2.getAuthInstance().signIn();
+  // Ideally the button should only show up after gapi.client.init finishes, so that this
+  // handler won't be called before OAuth is initialized.
+  gapi.auth2.getAuthInstance().signIn();
 }
 
 function handleSignOutClick(event) {
-gapi.auth2.getAuthInstance().signOut();
+  gapi.auth2.getAuthInstance().signOut();
 }
 
 function loadBoardList() {
-  profile = myProfile();
+  var profile = myProfile();
   gapi.client.sheets.spreadsheets.values.get({
     spreadsheetId: '1u9w_rAWrPBUnmQ_G4TYvnIEDifVQg4HWKhbqvFET2Yk',
     range: 'A1:Z9999',
   }).then(function(result) {
     console.log(result);
-    rows = result.result.values;
-    sizes = new Map();
+    var rows = result.result.values;
+    var sizes = new Map();
     sizes.set("unsolved", new Map());
     sizes.set("solvedBySomeone", new Map());
     sizes.set("solvedByMe", new Map());
+    document.getElementById("unsolved").innerHTML = "";
+    document.getElementById("solvedBySomeone").innerHTML = "";
+    document.getElementById("solvedByMe").innerHTML = "";
     for (i=0; i<rows.length; ++i) {
-      b = document.createElement("button");
+      var b = document.createElement("button");
       b.innerHTML = "Load board " + (i + 1);
       b.dataset.json = rows[i][0];
       b.dataset.sheetRow = i+1;
       b.dataset.playable = true;
-      b.addEventListener("click", setLettersFromDataset);
-      kind = "unsolved";
+      b.addEventListener("click", startGame);
+      var kind = "unsolved";
       if (rows[i].length > 1) {
         kind = "solvedBySomeone";
-        for (j=1; j<rows[i].length; ++j) {
+        for (j = 1; j < rows[i].length; ++j) {
           if (rows[i][j] == profile.getEmail()) {
             kind = "solvedByMe";
             b.dataset.playable = false;
           }
         }
       }
-      currentSizes = sizes.get(kind);
-      currentDiv = document.getElementById(kind);
-      parsedBoard = JSON.parse(b.dataset.json);
-      sizeAndTime = "Size " + parsedBoard.size + " time " + parsedBoard.timeSeconds;
+      var currentSizes = sizes.get(kind);
+      var currentDiv = document.getElementById(kind);
+      var parsedBoard = JSON.parse(b.dataset.json);
+      var sizeAndTime = "Size " + parsedBoard.size + " time " + parsedBoard.timeSeconds;
       if (!currentSizes.has(sizeAndTime)) {
-        titleDiv = document.createElement("div");
+        var titleDiv = document.createElement("div");
         titleDiv.innerHTML = sizeAndTime;
         currentDiv.appendChild(titleDiv);
         contentDiv = document.createElement("div");
@@ -80,37 +84,40 @@ function loadBoardList() {
 }
 
 function getBoardResults() {
-  range = "A" + sheetRow + ":Z" + sheetRow;
+  var range = "A" + szk.sheetRow + ":Z" + szk.sheetRow;
   gapi.client.sheets.spreadsheets.values.get({
       spreadsheetId: '1u9w_rAWrPBUnmQ_G4TYvnIEDifVQg4HWKhbqvFET2Yk',
       range: range,
     }).then(function(result) {
       console.log(result);
-      rendered = "";
+      var list = document.getElementById("boardResults");
+      list.innerHTML = "";
       row = result.result.values[0];
       for (i = 1; i < row.length; i += 4) {
-        rendered += "<br><div>email: " + row[i] + " name: " + row[i+1] + " score: " + row[i+2] + " words found: " + row[i+3] + "</div>";
+        var r = document.createElement("li");
+        r.innerHTML = row[i+1] + " &lt" + row[i] + "&gt: <br>Score: " + row[i+2]  + "<br>Words found: " + row[i+3];
+        list.appendChild(r);
       }
-      updateBoardResults(rendered);
+      document.getElementById("allWords").innerHTML = szk.boardData.words.join(", ");
   });
 }
 
 function saveBoardResult() {
   console.log("Saving...");
-  profile = myProfile();
+  var profile = myProfile();
   console.log(profile);
 
-  range = "A" + sheetRow + ":Z" + sheetRow;
-  emptyColIndex = -1;
+  var range = "A" + szk.sheetRow + ":Z" + szk.sheetRow;
+  szk.emptyColIndex = -1;
   gapi.client.sheets.spreadsheets.values.get({
       spreadsheetId: '1u9w_rAWrPBUnmQ_G4TYvnIEDifVQg4HWKhbqvFET2Yk',
       range: range,
     }).then(function(result) {
       console.log(result);
-      emptyColIndex = result.result.values[0].length;
-      console.log(emptyColIndex);
+      szk.emptyColIndex = result.result.values[0].length;
+      console.log(szk.emptyColIndex);
   }).then(function() {
-      range = String.fromCharCode("A".charCodeAt(0) + emptyColIndex) + sheetRow;
+      var range = String.fromCharCode("A".charCodeAt(0) + szk.emptyColIndex) + szk.sheetRow;
 
       console.log(range);
       gapi.client.sheets.spreadsheets.values.update({
@@ -119,7 +126,7 @@ function saveBoardResult() {
         valueInputOption: "RAW",
         resource: {
           values: [
-            [profile.getEmail(), profile.getName(), score, found_words.join()],
+            [profile.getEmail(), profile.getName(), szk.score, szk.found_words.join(", ")],
           ],
         }
       }).then(function(err, result) {
