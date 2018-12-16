1f84202c93a0e2e66a524a549baeda888d218d21
László Lukács <lukacs@google.com>
2018-12-16 16:35:03 +0100

    Add logging

--- web/service-worker.js
+++ web/service-worker.js
@@ -9,6 +9,7 @@ var urlsToCache = [
 
 self.addEventListener('install', function(event) {
   // Perform install steps
+  console.log("Installing service worker");
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
@@ -19,6 +20,7 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
+  console.log("Fetching in service worker");
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
