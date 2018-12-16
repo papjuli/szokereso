97e8c1489a7fa5a53928085dfb3328174355a5fe
László Lukács <lukacs@google.com>
2018-12-16 16:49:31 +0100

    Fix syntax error

--- web/service-worker.js
+++ web/service-worker.js
@@ -1,2 +1,2 @@
-self.addEventListener('install', function(event) { console.log("Install in service worker"}));
-self.addEventListener('fetch', function(event) { console.log("Fetch in service worker")});
+self.addEventListener('install', function(event) { console.log("Install in service worker"); });
+self.addEventListener('fetch', function(event) { console.log("Fetch in service worker"); });
